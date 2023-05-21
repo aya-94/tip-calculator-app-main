aaf43236411afb513dd46c14bef325bc65dc3383
hoday <hodayamirr@gmail.com>
2023-05-21 12:27:06 +0300

    fixing code issues

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const testObject = [];
 const button = document.getElementById("button");
 const bill = document.getElementById("bill");
 const customPrecent = document.getElementById("custom-precent");
 const people = document.getElementById("num-ppl");
 const tipAmount = document.getElementById("tip-amount");
 const totalAmount = document.getElementById("total");
+const errorSpan = document.querySelector(".error-span");
 const val5 = document.getElementById("val-5");
 const val10 = document.getElementById("val-10");
 const val15 = document.getElementById("val-15");
@@ -14,6 +14,7 @@ const val25 = document.getElementById("val-25");
 const val50 = document.getElementById("val-50");
 const precentsArray = document.querySelectorAll(".precent");
 let tipPrecent;
+let isReset = false;
 val5.value = 5;
 val10.value = 10;
 val15.value = 15;
@@ -29,78 +30,81 @@ val50.value = 50;
             if(element !== customPrecent) {
                 element.classList.add("active");
             }
-            console.log(element == customPrecent)
             if(element.value > 0) {
                 tipPrecent = element.value
-                console.log(element.value)
             } else {
                 customPrecent.addEventListener("keyup", () => {
                     tipPrecent = customPrecent.value;
                 })
             }
+            console.log(tipPrecent)
         })
     });
   }
-
   precentVal();
 
   const billVal = () => {
-    if(bill.value < 1){
-        console.log('cannot be 0')
-    } else {
-        return Number(bill.value);
-    }
+    return Number(bill.value);
   }
 
   const peopleVal = () => {
-    if(people.value < 1){
+    if(people.value <= 0){
         console.log('cannot be 0')
     } else {
         return Number(people.value);
     }
   }
 
-  const calculateTip = () => {
-    let tip = testObject[0].bill * (testObject[0].customPrecent * 0.01)
-    let tipResults = (tip / testObject[0].people).toFixed(2)
+  const calculateTip = (bill, customPrecent, people) => {
+    let tip = bill * (customPrecent * 0.01)
+    let tipResults = (tip / people).toFixed(2)
     console.log(tipResults)
     tipAmount.innerHTML = `$${tipResults}`
   }
 
 
-  const calculateTotal = () => {
-    let total = testObject[0].bill + (testObject[0].bill * (testObject[0].customPrecent * 0.01))
-    let totalResults = (total / testObject[0].people).toFixed(2)
+  const calculateTotal = (bill, customPrecent, people) => {
+    let total = bill + (bill * (customPrecent * 0.01))
+    let totalResults = (total / people).toFixed(2)
     console.log(totalResults)
     totalAmount.innerHTML = `$${totalResults}`
   }
 
-  button.addEventListener("click", function() {
-    if(billVal() && peopleVal()) {
-        testObject.push({
-        bill: billVal(),
-        customPrecent: Number(tipPrecent),
-        people: peopleVal()
-        })
-        console.log(testObject)
-    } else {
-        console.log('error')
-    }
-    calculateTip()
-    calculateTotal()
+  const inputError = () => {
+    people.classList.add("error-input-active");
+    errorSpan.classList.add("error-span-active");
+  }
 
-    button.addEventListener("click", function() {
+  button.addEventListener("click", function() {
+    console.log(tipPrecent)
+    people.classList.remove("error-input-active");
+    errorSpan.classList.remove("error-span-active");
+    if(!isReset) {
+        if(billVal() && peopleVal()) {
+            calculateTip(billVal(), Number(tipPrecent), peopleVal())
+            calculateTotal(billVal(), Number(tipPrecent), peopleVal())
+            isReset = true;
+        } else {
+            console.log('error')
+            inputError()
+        }       
+    } else {
         bill.value = '';
         people.value = '';
+        customPrecent.value = '';
         tipAmount.innerHTML = '$0.00';
         totalAmount.innerHTML = '$0.00';
+        people.classList.remove("error-input-active");
+        errorSpan.classList.remove("error-span-active");
 
         precentsArray.forEach(element => {
             precentsArray.forEach(element => {
                 element.classList.remove("active");
             })
         })
-      })
+        isReset = false; 
+    }
+    // isReset = !isReset;
   })
 
   
